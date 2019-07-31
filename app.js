afc6cdd7cb3826bbf0e1299bfd765e4d0b07a956
Sergio Festa <sfestatomari@gmail.com>
2019-07-31 15:38:25 -0300

    add comments to auth

--- app.js
+++ app.js
@@ -41,30 +41,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
-app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true,
-}));
-
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
-hbs.registerHelper('ifUndefined', (value, options) => {
-  if (arguments.length < 2)
-      throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
-  if (typeof value !== undefined ) {
-      return options.inverse(this);
-  } 
-      return options.fn(this);
-  
-});
-
 
 // Enable authentication using session + passport
 app.use(session({
