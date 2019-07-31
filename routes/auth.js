fa0b7f2ff393620c7cd487d3adfb60608dc6af38
Sergio Festa <sfestatomari@gmail.com>
2019-07-31 11:59:58 -0300

    add authentication

--- routes/auth.js
+++ routes/auth.js
@@ -1,39 +1,36 @@
-const express = require("express");
+const express = require('express');
 const passport = require('passport');
+
 const router = express.Router();
-const User = require("../models/User");
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
 
 // Bcrypt to encrypt passwords
-const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
 
-router.get("/login", (req, res, next) => {
-  res.render("auth/login", { "message": req.flash("error") });
-});
+router.post('/signup', (req, res) => {
+  const { username, password } = req.body;
 
-router.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/auth/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
-
-router.get("/signup", (req, res, next) => {
-  res.render("auth/signup");
-});
-
-router.post("/signup", (req, res, next) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  if (username === "" || password === "") {
-    res.render("auth/signup", { message: "Indicate username and password" });
+  if (!username || !password) {
+    res.status(400).json({ message: 'Provide username and password' });
     return;
   }
 
-  User.findOne({ username }, "username", (err, user) => {
-    if (user !== null) {
-      res.render("auth/signup", { message: "The username already exists" });
+  if (password.length < 7) {
+    res.status(400).json({ message: 'Please make your password at least 8 characters long for security purposes.' });
+    return;
+  }
+
+  User.findOne({ username }, 'username', (err, user) => {
+
+    if (err) {
+      res.status(500).json({ message: 'Username check went bad.' });
+      return;
+    }
+
+    if (user) {
+      res.status(400).json({ message: 'Username taken. Choose another one.' });
       return;
     }
 
@@ -42,22 +39,71 @@ router.post("/signup", (req, res, next) => {
 
     const newUser = new User({
       username,
-      password: hashPass
+      password: hashPass,
     });
 
-    newUser.save()
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
+    newUser.save((error) => {
+      if (error) {
+        res.status(400).json({ message: 'Saving user to database went wrong.' });
+        return;
+      }
+
+      // Automatically log in user after sign up
+      // .login() here is actually predefined passport method
+      req.login(newUser, (error) => {
+        if (err) {
+          res.status(500).json({ message: 'Login after signup went bad.' });
+          return;
+        }
+
+        // Send the user's information to the frontend
+        // We can use also: res.status(200).json(req.user);
+        res.status(200).json(newUser);
+      });
+    });
   });
 });
 
-router.get("/logout", (req, res) => {
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, theUser, failureDetails) => {
+    if (err) {
+      res.status(500).json({ message: 'Something went wrong authenticating user' });
+      return;
+    }
+
+    if (!theUser) {
+      // "failureDetails" contains the error messages
+      // from our logic in "LocalStrategy" { message: '...' }.
+      res.status(401).json(failureDetails);
+      return;
+    }
+
+    // save user in session
+    req.login(theUser, (err) => {
+      if (err) {
+        res.status(500).json({ message: 'Session save went bad.' });
+        return;
+      }
+
+      // We are now logged in (that's why we can also send req.user)
+      res.status(200).json(theUser);
+    });
+  })(req, res, next);
+});
+
+router.post('/logout', (req, res) => {
+  // req.logout() is defined by passport
   req.logout();
-  res.redirect("/");
+  res.status(200).json({ message: 'Log out success!' });
+});
+
+
+router.get('/loggedin', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.status(200).json(req.user);
+    return;
+  }
+  res.status(403).json({ message: 'Unauthorized' });
 });
 
 module.exports = router;
